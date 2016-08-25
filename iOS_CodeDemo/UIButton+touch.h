020393b813832775c236ef30fe9be0225bccee42
On1Key <macbook@macPro.local>
2016-08-25 14:00:55 +0800

    添加UIButton+touch类扩展，防止多次点击按钮

---
+++ iOS_CodeDemo/UIButton+touch.h
@@ -0,0 +1,14 @@
+//
+//  UIButton+touch.h
+//  data
+//
+//  Created by mac book on 16/8/25.
+//  Copyright © 2016年 mac book. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (touch)
+/**设置点击时间间隔*/
+@property (nonatomic, assign) NSTimeInterval timeInterval;
+@end
